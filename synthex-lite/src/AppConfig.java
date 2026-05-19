44ad8d6819761f8482f0f7406e9283af49c34190
Maryam Naeem <maryam.naeem.iqbal@gmail.com>
2026-05-19 12:58:21 +0500

    Update AppConfig.java

--- synthex-lite/src/AppConfig.java
+++ synthex-lite/src/AppConfig.java
@@ -1,9 +1,9 @@
 package com.synthex;
 
 public class AppConfig {
-    public static final String SQLITE_DB_PATH = env("SYNTHEX_SQLITE_PATH", "data/lite.db");
-    public static final String FIREBASE_SERVICE_ACCOUNT_PATH = env("SYNTHEX_FIREBASE_SERVICE_ACCOUNT_PATH", "C:\\SDA Tasks\\Synthex\\synthex-e76bb-firebase-adminsdk-fbsvc-4529fbf21c.json");
-    public static final String FIREBASE_API_KEY = env("SYNTHEX_FIREBASE_API_KEY", "AIzaSyAkUTgKOzkXcpbW0UD4LUpOveJSDuzxvpQ");
+    public static final String SQLITE_DB_PATH = env(---);
+    public static final String FIREBASE_SERVICE_ACCOUNT_PATH = env("---", "---");
+    public static final String FIREBASE_API_KEY = env("----", "----");
     public static final String OLLAMA_MODEL = env("OLLAMA_MODEL", "qwen2.5-coder:3b");
 
     private static String env(String key, String defaultVal) {
